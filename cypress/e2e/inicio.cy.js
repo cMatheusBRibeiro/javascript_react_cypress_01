1c82244b85cfeae4932988db5296dfa202448363
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-01-10 22:23:50 -0300

    feat(Início): Criado novo comando de verificação de texto e implementado em nos testes de Início

--- cypress/e2e/inicio.cy.js
+++ cypress/e2e/inicio.cy.js
@@ -5,8 +5,8 @@ describe("Página inicial", () => {
 
   it("Deve verificar se o título principal está correto", () => {
     cy.visit("http://localhost:3000");
-    cy.getByTestId("titulo-principal").should(
-      "contain",
+    cy.verifyContent(
+      "titulo-principal",
       "Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!"
     );
   });
@@ -17,22 +17,19 @@ describe("Página inicial", () => {
     });
 
     it("Deve verificar se o título de vantagens está correto", () => {
-      cy.getByTestId("titulo-vantagens-do-banco").should(
-        "contain",
+      cy.verifyContent(
+        "titulo-vantagens-do-banco",
         "Vantagens do nosso banco:"
       );
     });
 
     describe("Conta e cartões gratuitos", () => {
       it("Deve verificar se o título está correto", () => {
-        cy.getByTestId("titulo-conta-cartoes").should(
-          "contain",
-          "Conta e cartão gratuitos"
-        );
+        cy.verifyContent("titulo-conta-cartoes", "Conta e cartão gratuitos");
       });
       it("Deve verificar se o conteúdo está correto", () => {
-        cy.getByTestId("conteudo-conta-cartoes").should(
-          "contain",
+        cy.verifyContent(
+          "conteudo-conta-cartoes",
           "Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção."
         );
       });
@@ -40,14 +37,11 @@ describe("Página inicial", () => {
 
     describe("Saques sem custo", () => {
       it("Deve verificar se o título está correto", () => {
-        cy.getByTestId("titulo-saques-custo").should(
-          "contain",
-          "Saques sem custo"
-        );
+        cy.verifyContent("titulo-saques-custo", "Saques sem custo");
       });
       it("Deve verificar se o conteúdo está correto", () => {
-        cy.getByTestId("conteudo-saques-custo").should(
-          "contain",
+        cy.verifyContent(
+          "conteudo-saques-custo",
           "Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h."
         );
       });
@@ -55,28 +49,22 @@ describe("Página inicial", () => {
 
     describe("Programa de pontos", () => {
       it("Deve verificar se o título está correto", () => {
-        cy.getByTestId("titulo-programa-pontos").should(
-          "contain",
-          "Programa de pontos"
-        );
+        cy.verifyContent("titulo-programa-pontos", "Programa de pontos");
       });
       it("Deve verificar se o conteúdo está correto", () => {
-        cy.getByTestId("conteudo-programa-pontos").should(
-          "contain",
+        cy.verifyContent(
+          "conteudo-programa-pontos",
           "Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!"
         );
       });
     });
     describe("Seguro Dispositivos", () => {
       it("Deve verificar se o título está correto", () => {
-        cy.getByTestId("titulo-seguro-dispositivos").should(
-          "contain",
-          "Seguro Dispositivos"
-        );
+        cy.verifyContent("titulo-seguro-dispositivos", "Seguro Dispositivos");
       });
       it("Deve verificar se o conteúdo está correto", () => {
-        cy.getByTestId("conteudo-seguro-dispositivos").should(
-          "contain",
+        cy.verifyContent(
+          "conteudo-seguro-dispositivos",
           "Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica."
         );
       });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,7 @@
 Cypress.Commands.add("getByTestId", (selector) => {
   return cy.get(`[data-testid="${selector}"]`);
 });
+
+Cypress.Commands.add("verifyContent", (selector, text) => {
+  return cy.getByTestId(selector).should("contain", text);
+});
