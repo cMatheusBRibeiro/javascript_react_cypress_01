bf2831be659e2892285f44cd3f67ee4b3c15502e
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-01-10 21:53:58 -0300

    feat(Início): Adicionado data-testid e incrementando testes dessa página

--- cypress/e2e/inicio.cy.js
+++ cypress/e2e/inicio.cy.js
@@ -1,43 +1,59 @@
-describe("Início", () => {
+describe("Página inicial", () => {
   it("Deve acessar a página inicial da aplicação", () => {
     cy.visit("http://localhost:3000");
   });
-});
 
-describe("Vantagens da Bytebank", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000");
-  });
+  describe("Vantagens da Bytebank", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000");
+    });
 
-  it("Deve verificar se o título está correto", () => {
-    cy.contains("Vantagens do nosso banco:");
-  });
+    it("Deve verificar se o título principal está correto", () => {
+      cy.get('[data-testid="titulo-principal"]').contains(
+        "Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!"
+      );
+    });
 
-  it("Deve verificar se o texto relacionado à Conta e cartões seguros está correto", () => {
-    cy.contains("Conta e cartão gratuitos");
-    cy.contains(
-      "Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção."
-    );
-  });
+    it("Deve verificar se o título de vantagens está correto", () => {
+      cy.get('[data-testid="titulo-vantagens-do-banco"]').contains(
+        "Vantagens do nosso banco:"
+      );
+    });
 
-  it("Deve verificar se o texto relacionado à Saques sem custo está correto", () => {
-    cy.contains("Saques sem custo");
-    cy.contains(
-      "Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h."
-    );
-  });
+    it("Deve verificar se o texto relacionado à Conta e cartões seguros está correto", () => {
+      cy.get('[data-testid="titulo-conta-cartoes"]').contains(
+        "Conta e cartão gratuitos"
+      );
+      cy.get('[data-testid="conteudo-conta-cartoes"]').contains(
+        "Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção."
+      );
+    });
 
-  it("Deve verificar se o texto relacionado à Programa de pontos está correto", () => {
-    cy.contains("Programa de pontos");
-    cy.contains(
-      "Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!"
-    );
-  });
+    it("Deve verificar se o texto relacionado à Saques sem custo está correto", () => {
+      cy.get('[data-testid="titulo-saques-custo"]').contains(
+        "Saques sem custo"
+      );
+      cy.get('[data-testid="conteudo-saques-custo"]').contains(
+        "Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h."
+      );
+    });
 
-  it("Deve verificar se o texto relacionado à Seguro Dispositivos está correto", () => {
-    cy.contains("Seguro Dispositivos");
-    cy.contains(
-      "Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica."
-    );
+    it("Deve verificar se o texto relacionado à Programa de pontos está correto", () => {
+      cy.get('[data-testid="titulo-programa-pontos"]').contains(
+        "Programa de pontos"
+      );
+      cy.get('[data-testid="conteudo-programa-pontos"]').contains(
+        "Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!"
+      );
+    });
+
+    it("Deve verificar se o texto relacionado à Seguro Dispositivos está correto", () => {
+      cy.get('[data-testid="titulo-seguro-dispositivos"]').contains(
+        "Seguro Dispositivos"
+      );
+      cy.get('[data-testid="conteudo-seguro-dispositivos"]').contains(
+        "Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica."
+      );
+    });
   });
 });
--- src/paginas/Inicio/index.jsx
+++ src/paginas/Inicio/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ilustracaoBanner from 'assets/ilustracao-banner.svg';
-import estilos from './Inicio.module.css';
-import presente from 'assets/presente.svg';
-import saque from 'assets/saque.svg';
-import estrela from 'assets/estrela.svg';
-import dispositivo from 'assets/dispositivo.svg';
+import React from "react";
+import ilustracaoBanner from "assets/ilustracao-banner.svg";
+import estilos from "./Inicio.module.css";
+import presente from "assets/presente.svg";
+import saque from "assets/saque.svg";
+import estrela from "assets/estrela.svg";
+import dispositivo from "assets/dispositivo.svg";
 
 export default function Inicio() {
   return (
@@ -12,7 +12,7 @@ export default function Inicio() {
       <div className={estilos.container}>
         <div className={estilos.conteudo}>
           <div className={estilos.banner}>
-            <h1 data-test="titulo-principal">
+            <h1 data-testid="titulo-principal">
               Experimente mais liberdade no controle da sua vida financeira.
               Crie sua conta com a gente!
             </h1>
@@ -22,28 +22,32 @@ export default function Inicio() {
             />
           </div>
           <div className={estilos.vantagens}>
-            <h2>Vantagens do nosso banco:</h2>
+            <h2 data-testid="titulo-vantagens-do-banco">
+              Vantagens do nosso banco:
+            </h2>
             <div className={estilos.vantagens__itens}>
               <div className={estilos.vantagens__item}>
                 <img src={presente} alt="ícone de um presente" />
-                <h3>Conta e cartão gratuitos</h3>
-                <p>
+                <h3 data-testid="titulo-conta-cartoes">
+                  Conta e cartão gratuitos
+                </h3>
+                <p data-testid="conteudo-conta-cartoes">
                   Isso mesmo, nossa conta é digital, sem custo fixo e mais que
                   isso: sem tarifa de manutenção.
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
                 <img src={saque} alt="ícone de uma mão segurando um dinheiro" />
-                <h3>Saques sem custo</h3>
-                <p>
+                <h3 data-testid="titulo-saques-custo">Saques sem custo</h3>
+                <p data-testid="conteudo-saques-custo">
                   Você pode sacar gratuitamente 4x por mês de qualquer Banco
                   24h.
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
                 <img src={estrela} alt="ícone de uma estrela" />
-                <h3>Programa de pontos</h3>
-                <p>
+                <h3 data-testid="titulo-programa-pontos">Programa de pontos</h3>
+                <p data-testid="conteudo-programa-pontos">
                   Você pode acumular pontos com suas compras no crédito sem
                   pagar mensalidade!
                 </p>
@@ -53,8 +57,10 @@ export default function Inicio() {
                   src={dispositivo}
                   alt="ícone de um dispositivo como notebook"
                 />
-                <h3>Seguro Dispositivos</h3>
-                <p>
+                <h3 data-testid="titulo-seguro-dispositivos">
+                  Seguro Dispositivos
+                </h3>
+                <p data-testid="conteudo-seguro-dispositivos">
                   Seus dispositivos móveis (computador e laptop) protegidos por
                   uma mensalidade simbólica.
                 </p>
