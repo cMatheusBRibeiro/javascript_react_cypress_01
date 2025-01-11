e0abde2f09ffeab9e1dc1d8fc2b7bf77f6e96ab2
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-01-10 21:58:59 -0300

    feat(Commands): Criado comando personalizado de getByTestId e implementado no inicio.cy.js

--- cypress/e2e/inicio.cy.js
+++ cypress/e2e/inicio.cy.js
@@ -9,49 +9,45 @@ describe("Página inicial", () => {
     });
 
     it("Deve verificar se o título principal está correto", () => {
-      cy.get('[data-testid="titulo-principal"]').contains(
+      cy.getByTestId("titulo-principal").contains(
         "Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!"
       );
     });
 
     it("Deve verificar se o título de vantagens está correto", () => {
-      cy.get('[data-testid="titulo-vantagens-do-banco"]').contains(
+      cy.getByTestId("titulo-vantagens-do-banco").contains(
         "Vantagens do nosso banco:"
       );
     });
 
     it("Deve verificar se o texto relacionado à Conta e cartões seguros está correto", () => {
-      cy.get('[data-testid="titulo-conta-cartoes"]').contains(
+      cy.getByTestId("titulo-conta-cartoes").contains(
         "Conta e cartão gratuitos"
       );
-      cy.get('[data-testid="conteudo-conta-cartoes"]').contains(
+      cy.getByTestId("conteudo-conta-cartoes").contains(
         "Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção."
       );
     });
 
     it("Deve verificar se o texto relacionado à Saques sem custo está correto", () => {
-      cy.get('[data-testid="titulo-saques-custo"]').contains(
-        "Saques sem custo"
-      );
-      cy.get('[data-testid="conteudo-saques-custo"]').contains(
+      cy.getByTestId("titulo-saques-custo").contains("Saques sem custo");
+      cy.getByTestId("conteudo-saques-custo").contains(
         "Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h."
       );
     });
 
     it("Deve verificar se o texto relacionado à Programa de pontos está correto", () => {
-      cy.get('[data-testid="titulo-programa-pontos"]').contains(
-        "Programa de pontos"
-      );
-      cy.get('[data-testid="conteudo-programa-pontos"]').contains(
+      cy.getByTestId("titulo-programa-pontos").contains("Programa de pontos");
+      cy.getByTestId("conteudo-programa-pontos").contains(
         "Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!"
       );
     });
 
     it("Deve verificar se o texto relacionado à Seguro Dispositivos está correto", () => {
-      cy.get('[data-testid="titulo-seguro-dispositivos"]').contains(
+      cy.getByTestId("titulo-seguro-dispositivos").contains(
         "Seguro Dispositivos"
       );
-      cy.get('[data-testid="conteudo-seguro-dispositivos"]').contains(
+      cy.getByTestId("conteudo-seguro-dispositivos").contains(
         "Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica."
       );
     });
